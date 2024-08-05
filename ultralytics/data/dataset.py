b82a29e74f3c5f19e2c32cb449bcce3ad8637fd1
Kevin Agusto <kevinagusto28@gmail.com>
2024-08-05 16:48:13 +0700

    fix bug

--- ultralytics/data/dataset.py
+++ ultralytics/data/dataset.py
@@ -442,7 +442,7 @@ class ClassificationDataset:
 
         root_type = os.path.split(root)[1]
         root = os.path.split(root)[0]
-        all_dataset_paths = [root]+list(args.rehearsal_replay_paths or [])
+        all_dataset_paths = [root]+list(args.rehearsal_replay_paths if not (args.rehearsal_replay_paths is None) else [])
         all_image_folders = []
         
         for data_path in all_dataset_paths:
